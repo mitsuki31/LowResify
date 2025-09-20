a5f827ca5b6a1a0081b9c3dd0cfa335e759fb06c
Ryuu Mitsuki <dhefam31@gmail.com>
2025-09-21 00:32:22 +0700

    fix(pwa): Remove network-first strategy

--- public/sw.js
+++ public/sw.js
@@ -13,36 +13,33 @@ self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
 
   event.respondWith(
-    // Fetch first, fallback to cache if no connection
-    fetch(event.request).catch(() => {
-      caches.open(CACHE_NAME).then(async cache => {
-        const cached = await cache.match(event.request);
-        if (cached) {
-          // Check cache age
-          const dateHeader = cached.headers.get('sw-cache-time');
-          if (dateHeader) {
-            const age = (Date.now() - Number(dateHeader)) / 1000;
-            if (age < CACHE_MAX_AGE) return cached;
-          }
-          // If too old, fetch new
+    caches.open(CACHE_NAME).then(async cache => {
+      const cached = await cache.match(event.request);
+      if (cached) {
+        // Check cache age
+        const dateHeader = cached.headers.get('sw-cache-time');
+        if (dateHeader) {
+          const age = (Date.now() - Number(dateHeader)) / 1000;
+          if (age < CACHE_MAX_AGE) return cached;
         }
-        try {
-          const response = await fetch(event.request);
-          if (response.ok) {
-            // Clone and add a custom header for cache time
-            const cloned = response.clone();
-            const headers = new Headers(cloned.headers);
-            headers.append('sw-cache-time', Date.now().toString());
-            const body = await cloned.blob();
-            const newResponse = new Response(body, { status: cloned.status, statusText: cloned.statusText, headers });
-            cache.put(event.request, newResponse.clone());
-            return newResponse;
-          }
-          return response;
-        } catch (e) {
-          return cached || Response.error();
+        // If too old, fetch new
+      }
+      try {
+        const response = await fetch(event.request);
+        if (response.ok) {
+          // Clone and add a custom header for cache time
+          const cloned = response.clone();
+          const headers = new Headers(cloned.headers);
+          headers.append('sw-cache-time', Date.now().toString());
+          const body = await cloned.blob();
+          const newResponse = new Response(body, { status: cloned.status, statusText: cloned.statusText, headers });
+          cache.put(event.request, newResponse.clone());
+          return newResponse;
         }
-      })
+        return response;
+      } catch (e) {
+        return cached || Response.error();
+      }
     })
   );
 });
